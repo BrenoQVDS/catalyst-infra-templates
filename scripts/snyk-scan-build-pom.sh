ffb371d86e14c8bde70dbbdf65475bc115d23fbc
Breno Queiroz <brenoqueirozvds@gmail.com>
2023-04-02 07:17:34 -0300

    Removing template

--- scripts/snyk-scan-build-pom.sh
+++ scripts/snyk-scan-build-pom.sh
@@ -1,44 +1,55 @@
 #!/bin/bash
 
-#~
+rm pom.xml
+touch pom.xml
 
-INDEP=false
-INEXC=false
+IN_DEPENDENCIES=false
+IN_EXCLUSIONS=false
+IN_PARENT=false
+IN_MODULES=false
 DEPS=""
 
-while IFS= read -r ln; do
-
-  [[ "$ln" == *"</dependencies>"* ]] && INDEP=false
-  [[ "$ln" == *"<exclusions>"* ]] && INEXC=true
-
-  $INDEP && ! $INEXC && DEPS+="${ln:4}"$'\n'
-
-  [[ "$ln" == *"</exclusions>"* ]] && INEXC=false
-  [[ "$ln" == *"<dependencies>"* ]] && INDEP=true
-
-done < "../pom.xml"
-
-#~
-
-DF="pom.xml"
-
-echo "test 10.0"
-
-echo $DEPS
-
-echo "test 10.1"
-rm "$DF"
-echo "test 10.2"
-touch "$DF"
-echo "test 10.3"
+PARENT_ARTIFACT=$(mvn -f ../pom.xml -q -Dexec.executable=echo -Dexec.args='${project.artifactId}' --non-recursive exec:exec 2>/dev/null)
+PARENT_VERSION=$(mvn -f ../pom.xml -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec 2>/dev/null)
 
 while IFS= read -r ln; do
 
-  if [[ "$ln" == *"<dependencies>"* ]]; then
-    echo "$ln" >> "$DF"
-    echo "$DEPS" >> "$DF"
-  else
-    echo "$ln" >> "$DF"
+  #Reorder this
+  [[ "$ln" == *"</dependencies>"* ]] && IN_DEPENDENCIES=false
+  [[ "$ln" == *"<exclusions>"* ]] && IN_EXCLUSIONS=true
+  [[ "$ln" == *"<parent>"* ]] && IN_PARENT=true
+  [[ "$ln" == *"<modules>"* ]] && IN_MODULES=true
+
+  $IN_DEPENDENCIES && ! $IN_EXCLUSIONS && DEPS+="${ln:4}"$'\n'
+  
+  if [[ $IN_PARENT = false && $IN_MODULES = false && $IN_DEPENDENCIES = false ]]
+  then
+    if [[ "$ln" == *"<artifactId>app-engine</artifactId>"* ]]
+    then
+      echo "    <artifactId>snyc-scan-project</artifactId>" >> pom.xml
+    elif [[ "$ln" == *"</project>"* ]]
+    then    
+      echo "<dependencies>" >> pom.xml
+      echo $DEPS >> pom.xml
+      echo "</dependencies>" >> pom.xml
+      echo "</project>" >> pom.xml
+    else
+      echo "$ln" >> pom.xml
+    fi
   fi
 
-done < "snyc-scan-pom.tpl"
+  [[ "$ln" == *"</exclusions>"* ]] && IN_EXCLUSIONS=false
+  [[ "$ln" == *"<dependencies>"* ]] && IN_DEPENDENCIES=true
+  [[ "$ln" == *"</modules>"* ]] && IN_MODULES=false
+  if [[ "$ln" == *"</parent>"* ]]
+  then
+    IN_PARENT=false
+    echo "    <parent>" >> pom.xml
+    echo "        <groupId>org.entando</groupId>" >> pom.xml
+    echo "        <artifactId>$PARENT_ARTIFACT</artifactId>" >> pom.xml
+    echo "        <relativePath>../pom.xml</relativePath>" >> pom.xml
+    echo "        <version>$PARENT_VERSION</version>" >> pom.xml
+    echo "    </parent>" >> pom.xml
+   fi
+
+done < "../pom.xml"
